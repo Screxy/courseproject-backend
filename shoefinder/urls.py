5324f969f89267b711f1bde25501061a5d93b4a6
Screxy <dvbvladis@mail.ru>
2023-07-01 20:18:08 +0900

    added finder

--- shoefinder/urls.py
+++ shoefinder/urls.py
@@ -7,4 +7,6 @@ app_name = 'shoefinder'
 urlpatterns = [
     path("", views.index, name="index"),
     path("<int:question_id>/", views.detail, name="detail"),
+    path("polls", views.polls, name="polls"),
+    path("find", views.find, name="find"),
 ]
--- shoefinder/views.py
+++ shoefinder/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-
-from .models import ShoeModels, PurchaseLinks
+from django.db.models import Q
+from .models import ShoeModels, PurchaseLinks, Brand, Colors, Styles
 
 
 def index(request):
@@ -16,3 +16,43 @@ def detail(request, question_id):
     except ShoeModels.DoesNotExist:
         raise Http404("ShoeModels does not exist")
     return render(request, "shoefinder/detail.html", {"model_name": model_name, 'links': links})
+
+
+def polls(request):
+    try:
+        brands = Brand.objects.all();
+        styles = Styles.objects.all();
+        colors = Colors.objects.all();
+        context = {'brands': brands, 'styles': styles, 'colors': colors}
+    except:
+        raise Http404("Ошибка")
+    return render(request, "shoefinder/polls.html", context)
+
+
+def find(request):
+    selected_brands = request.POST.getlist('choiceBrands')
+    selected_colors = request.POST.getlist('choiceColors')
+    selected_styles = request.POST.getlist('choiceStyles')
+
+    query = Q()
+
+    for brand in selected_brands:
+        query |= Q(brand__name=brand)
+
+    for color in selected_colors:
+        query &= Q(shoecolors__color__name=color)
+
+    for style in selected_styles:
+        query &= Q(shoestyles__style__name=style)
+
+    shoe_models = ShoeModels.objects.filter(query).distinct()
+
+    context = {
+        'brands': selected_brands,
+        'styles': selected_styles,
+        'colors': selected_colors,
+        'models': shoe_models
+    }
+
+    return render(request, "shoefinder/findedShoes.html", context)
+#
